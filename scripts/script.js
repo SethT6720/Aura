4f8bf6605f01b7e218d0f2c7b1e037431504b19d
SethT6720 <101337407+SethT6720@users.noreply.github.com>
2024-08-09 14:10:38 +0000

    try this

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,10 @@
 import { get, pageShowing, hs } from './help.js';
 
-async function main() {
+const startPage = get('startPage');
+const startButton = get('startButton');
+const gamePage = get('gamePage');
 
-    const sleep = (ms) => new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-
-    sleep(1000);
-
-    const startPage = get('startPage');
-    const startButton = get('startButton');
-    const gamePage = get('gamePage');
-
-    startButton.addEventListener('click', function x() {
-        hs(gamePage, 'switch');
-        startButton.removeEventListener('click', x);
-    });
-
-}
-
-window.onload(main);
+startButton.addEventListener('click', function x() {
+    hs(gamePage, 'switch');
+    startButton.removeEventListener('click', x);
+});
