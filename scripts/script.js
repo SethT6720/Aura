9f0ab3013200d88fe4e2109752cb1d0a9a56be55
SethT6720 <101337407+SethT6720@users.noreply.github.com>
2024-08-10 22:35:05 +0000

    Added a counter for the currency (Aura is the current placeholder)

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,39 @@
 import { get, pageShowing, hs, createEle } from './help.js';
 
+//Delay for async funcs
+const sleep = (ms) => new Promise((resolve) => {
+    setTimeout(resolve, ms);
+});
+
+//Decalre element variables
 const startPage = get('startPage');
 const startButton = get('startButton');
 const gamePage = get('gamePage');
+const numberCounter = get('numberCounter');
 
+
+//Declare game variables
+let number = 10;
+
+
+//Start button listener
 startButton.addEventListener('click', function x() {
     hs(gamePage, 'switch');
     startButton.removeEventListener('click', x);
 });
+
+//Function to update all counters
+function updateCounters() {
+    numberCounter.innerHTML = number;
+}
+
+
+
+async function main() {
+    while (true) {
+        updateCounters();
+        await sleep(100);
+    }
+}
+
+main();
