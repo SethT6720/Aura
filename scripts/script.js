dd8c6ea36c3a796636e9f5434586725c6c3a0fa0
SethT6720 <101337407+SethT6720@users.noreply.github.com>
2024-09-10 14:35:16 +0000

    seeing the changes

--- scripts/script.js
+++ scripts/script.js
@@ -58,7 +58,7 @@ let dropsInStats = false;
 let respecShowing = false;
 
 //Declare game variables
-let aura = 0;
+let aura = 10;
 let auraPerMeditate = 1;
 let meditatePerSec = 0;
 let currentlyDoing = 'None'
